bbf0955e94e7ce8038f8739901f0911cc745bff7
Rbn-tok <itokianarabenantenaina@gmail.com>
2024-10-28 12:47:35 +0300

    ajout des champs

--- app/View/Components/register.blade.php
+++ app/View/Components/register.blade.php
@@ -1,39 +1,50 @@
-@extends('layouts.app')
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
 
-@section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">{{ __('Inscription') }}</div>
-
-            <div class="card-body">
-                <form method="POST" action="{{ route('register') }}">
-                    @csrf
-
-                    <div class="mb-3">
-                        <label for="name" class="form-label">{{ __('Nom') }}</label>
-                        <input id="name" type="text" class="form-control" name="name" required autofocus>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="email" class="form-label">{{ __('Email') }}</label>
-                        <input id="email" type="email" class="form-control" name="email" required>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="password" class="form-label">{{ __('Mot de passe') }}</label>
-                        <input id="password" type="password" class="form-control" name="password" required>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="password_confirmation" class="form-label">{{ __('Confirmer le mot de passe') }}</label>
-                        <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">{{ __('S\'inscrire') }}</button>
-                </form>
-            </div>
+        <!-- First Name -->
+        <div>
+            <x-input-label for="firstname" :value="__('First Name')" />
+            <x-text-input id="firstname" class="block mt-1 w-full" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="firstname" />
+            <x-input-error :messages="$errors->get('firstname')" class="mt-2" />
         </div>
-    </div>
-</div>
-@endsection
+
+        <!-- Last Name -->
+        <div class="mt-4">
+            <x-input-label for="lastname" :value="__('Last Name')" />
+            <x-text-input id="lastname" class="block mt-1 w-full" type="text" name="lastname" :value="old('lastname')" required autocomplete="lastname" />
+            <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
+        </div>
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
